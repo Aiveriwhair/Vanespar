cb282abe77af7086109be89e6b5753402684c24c
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2023-10-08 21:33:23 +0200

    init screen

--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -5,9 +5,54 @@ class StatsScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return(
-        const Text('Stats Screen : Not implemented Yet')
+    return Scaffold(
+      appBar: AppBar(title: const HeaderWidget(),
+      backgroundColor: Colors.black),
+      body: const Column(
+        children: <Widget>[
+          HabitFilterWidget(),
+          CalendarWidget()
+        ],
+      )
     );
   }
+}
 
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      color: Colors.black,
+      child: const Align(alignment: Alignment.centerLeft,
+      child: Text("Stats"),
+      )
+    );
+  }
+}
+
+class HabitFilterWidget extends StatefulWidget {
+  const HabitFilterWidget({super.key});
+  @override
+  State<StatefulWidget> createState() => _HabitFilterWidgetState();
+}
+class _HabitFilterWidgetState extends State<HabitFilterWidget>{
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+}
+
+class CalendarWidget extends StatefulWidget {
+  const CalendarWidget({super.key});
+  @override
+  State<StatefulWidget> createState() =>_CalendarWidgetState();
+}
+class _CalendarWidgetState extends State<CalendarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
 }
